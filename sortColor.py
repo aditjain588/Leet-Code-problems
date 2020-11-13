14fe36d9f357e992f2e129da1f10fb8834d683a8
aditjain588 <48388445+aditjain588@users.noreply.github.com>
2020-11-13 06:21:01 +0530

    Update and rename sortColor.py.txt to sortColor.py

--- sortColor.py
+++ sortColor.py
@@ -3,9 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #pointer i traversing the list, pointer start holding the next position for 0, pointer end holding the next position for 2
+        # if 0 encountered put it in starting of list, if 2 encountered put it in ending of list, value 1 will automatically will be in the middle
         start=0
         end=len(nums)-1
-        print(end)
         i=0
         
         while(i<=end):     
